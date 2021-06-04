d9301eba673732833f420c9ae40010ac5b798a77
Orress35 <79585248+Orress35@users.noreply.github.com>
2021-06-04 14:48:12 +0300

    Update Main.java

--- me/test/Main.java
+++ me/test/Main.java
@@ -6,7 +6,7 @@ public class Main {
             System.out.println("Invalid number of arguments. Expected: <file> <prefix> <suffix> <start> <end> <char/line>");
             return;
         }
-        Scala scala = new Scala();
-        scala.main(args[0], args[1], args[2], args[3], args[4], args[5]);
+        Formatter formatter = new Formatter();
+        formatter.main(args[0], args[1], args[2], args[3], args[4], args[5]);
     }
 }
